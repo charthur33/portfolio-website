af2fc2fac86b54a43c2487a277c4bbf73631e11c
Chris <c.william.arthur@gmail.com>
2022-03-19 15:18:08 -0700

    update intro section

--- src/components/intro/About.jsx
+++ src/components/intro/About.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import "./intro.css";
+import chrisPic from "../../images/chris.png";
 
 function About() {
     return (
         <section id="about-section">
-            <div className="about">
-
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6 about">
+                        <div className="intro-wrapper">
+                            <h2 className="intro-text">Hello, My name is</h2>
+                            <h1 className="i-name">Chris Arthur</h1>
+                            <div className="i-title">
+                                <div className="i-title-wrapper">
+                                    <div className="i-title-item">Engineer</div>
+                                    <div className="i-title-item">Software Developer</div>
+                                    <div className="i-title-item">Team Builder</div>
+                                    <div className="i-title-item">Product Designer</div>
+                                    <div className="i-title-item">Problem Solver</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 profile-pic">
+                        <img className="pic" src={chrisPic} alt="profile pic"></img>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- src/components/intro/Header.jsx
+++ src/components/intro/Header.jsx
@@ -5,17 +5,18 @@ function Header() {
 
     return (
         <header>
-            <h1 className="name">Chris Arthur</h1>
-            <nav className="navbar navbar-expand-md">
+            
+            <nav className="navbar navbar-expand-lg">
+            <a className="navbar-brand" href="#about-section">Chris Arthur</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         Menu 
-                        <i class="fa-solid fa-bars"> </i>
+                        <i className="fa-solid fa-bars"> </i>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav ms-auto">
                             <p className="nav-link" id="about" onClick={() => {
                                 const anchor = document.querySelector("#about-section");
-                                anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                                anchor.scrollIntoView({ behavior: 'smooth', block: 'start' });
                             }}
                             >About</p>
                             <p className="nav-link" id="projects" onClick={() => {
